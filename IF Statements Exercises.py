0d7bc5e67b0a0e3ad775865c97a41d5383907c62
koskinenj <jani23006@student.hamk.fi>
2023-11-02 14:35:04 +0200

    Update IF Statements Exercises.py

--- IF Statements Exercises.py
+++ IF Statements Exercises.py
@@ -1,7 +1,66 @@
-number1 = 10
-number2 = 5
+#  1
 
-if number1==number2:
-    print("Numbers are equal")
-else:
-    print("Numbers are not equal")    
+# number1 = 5
+# number2 = 3
+
+
+# if number1==number2:
+#     print("Numbers are equal")
+# else:
+#     print("Numbers are not equal")
+ 
+
+# if number1>number2:
+#     print("Number1 is greater")
+    
+# if number1>number2 or number1==number2:
+#     print("Number1 is greater than number2 or they are equal")
+    
+# if number1!=number2:
+#     print("Number1 is not equal to number2")
+     
+
+# 2
+
+
+# number1 = 5
+# number2 = 6
+# number3 = 5
+
+# if number1==number2==number3:
+#     print("All numbers are equal")
+    
+# if number1==number2 or number1==number3:
+#     print("Number1 and number2 are equal OR number1 and number3 are equal")
+    
+# if number1>number2 and number1>number3:
+#     print("Number1 is greater than number2 and number3 ")
+    
+# if number1 > number2:
+#     print("Number1 is greater than Number2")
+# elif number2>number3:
+#     print("Number2 is greater than Number3")
+    
+# if number1==number2:
+#     print("Number1 and number2 are equal")
+# elif number1==number3:
+#     print("Number1 and Number3 are equal")
+    
+
+# 3
+
+name1 = "Jani"
+name2 = "Eetu"
+name3 = "Teemu"
+
+
+if name1==name2:
+    print("Name1 and Name2 are equal")
+    
+if name1!=name2:
+    print("Name1 and Name2 are not equal")
+    
+if name1==name2:
+    print("Name1 and Name2 are equal")
+elif name1==name3:
+    print("Name1 and Name3 are equal")
